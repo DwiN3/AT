aeeaa898b8405604e03c4878f31894e14bf2804c
DwiN3 <dwinicht333@gmail.com>
2023-05-04 19:25:54 +0200

    Zmiana w currying

--- Paradygmaty programowania/Zadania/LAB/LAB10/currying.py
+++ Paradygmaty programowania/Zadania/LAB/LAB10/currying.py
@@ -2,10 +2,10 @@
 
 from functools import partial
 
-def curry(fun):
+def curryLambda(fun):
     return lambda x: lambda y: fun(x, y)
 
-def curry(fun):
+def curryPartial(fun):
     return partial(partial, fun)
 
 def add(x, y):
@@ -14,11 +14,13 @@ def add(x, y):
 def mul(x, y):
     return x * y
 
+def inc(x):
+    return curryPartial(add)(1)(x)
+
 print("Dodawanie przez funkcje = ", add(3, 5))
-print("Dodawanie przez curry   = ", curry(add)(3)(5))
+print("Dodawanie przez curry   = ", curryLambda(add)(3)(5))
 print("Mnożenie przez funkcje  = ", mul(3, 5))
-print("Mnożenie przez curry    = ", curry(mul)(3)(5))
+print("Mnożenie przez curry    = ", curryPartial(mul)(3)(5))
 
 x = 5
-inc = curry(add)(1)
-print(f"\nTestowa wartość ({x}) + 1 = ",inc(x))
+print(f"\nWartość ({x}) + 1 = ", inc(x))
